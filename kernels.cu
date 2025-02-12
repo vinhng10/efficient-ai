77fa91eb3c6642049e9c3d2181f30ba3043efdff
Vinh Nguyen <vinh.nguyen@finnos.fi>
2025-02-12 11:09:33 +0200

    update code

--- kernels.cu
+++ kernels.cu
@@ -10,48 +10,84 @@
 
 #define BLOCK_SIZE 16
 
+// Matrix stored in row-major order
+// M(row, col) = *(M.data + row * M.stride + col)
+typedef struct
+{
+  int height;
+  int width;
+  int stride;
+  float *data;
+} Matrix;
+
+// Get a matrix element at (row, col)
+__device__ float get_element(Matrix mat, int row, int col)
+{
+  return mat.data[row * mat.stride + col];
+}
+
+// Set a matrix element at (row, col)
+__device__ void set_element(Matrix mat, int row, int col, float value)
+{
+  mat.data[row * mat.stride + col] = value;
+}
+
+// Get a [BLOCK_SIZE x BLOCK_SIZE] sub-matrix of a matrix
+__device__ Matrix get_sub_matrix(Matrix mat, int blockRow, int blockCol)
+{
+  Matrix sub;
+  sub.height = BLOCK_SIZE;
+  sub.width = BLOCK_SIZE;
+  sub.stride = mat.stride;
+  sub.data = &mat.data[mat.stride * BLOCK_SIZE * blockRow + BLOCK_SIZE * blockCol];
+}
+
 inline int cdiv(int a, int b) { return (a + b - 1) / b; }
 
-__global__ void matmul_shared_kernel(
-    float *mat1, float *mat2, float *output,
-    int height, int width, int inner)
+__global__ void matmul_shared_kernel(Matrix A, Matrix B, Matrix C)
 {
-  // For each thread in a block:
-  int sr = threadIdx.y, sc = threadIdx.x;
-  int r = blockIdx.y * blockDim.y + threadIdx.y;
-  int c = blockIdx.x * blockDim.x + threadIdx.x;
+  // Block row and col:
+  int blockRow = blockIdx.y, blockCol = blockIdx.x;
+  // Thread row and col
+  int row = threadIdx.y, col = threadIdx.x;
+  // Matrix row and col
+  int r = blockRow * blockDim.y + row, c = blockCol * blockDim.x + col;
 
-  // Initialize shared memory for sub-matrices sub1 and sub2
-  __shared__ float shared1[BLOCK_SIZE][BLOCK_SIZE], shared2[BLOCK_SIZE][BLOCK_SIZE];
+  // Each block computes one sub-matrix Csub of C
+  Matrix Csub = get_sub_matrix(C, blockRow, blockCol);
 
-  // Initialize matmul accumulator
-  float acc = 0.0f;
+  // Each thread computes one element of Csub by
+  // by accumulating to Cvalue
+  float Cvalue = 0.0;
 
-  for (int s = 0; s < cdiv(inner, BLOCK_SIZE); ++s)
+  // Initialize shared memory for sub-matrices Asub and Bsub
+  __shared__ float Ashared[BLOCK_SIZE][BLOCK_SIZE], Bshared[BLOCK_SIZE][BLOCK_SIZE];
+
+  for (int s = 0; s < cdiv(A.width, BLOCK_SIZE); ++s)
   {
-    if (r >= height || c >= width)
-      return;
+    // Get Asub and Bsub sub-matrices
+    Matrix Asub = get_sub_matrix(A, blockRow, s);
+    Matrix Bsub = get_sub_matrix(A, s, blockCol);
 
-    // Load sub1 and sub2 from global memory to shared memory
+    // Load Asub and Bsub from global memory to shared memory
     // Each thread loads 1 element of each sub-matrix
-    int idx = s * BLOCK_SIZE;
-    shared1[sr][sc] = r < height && ? mat1[r * inner + s * BLOCK_SIZE + sc] : 0.0f;
-    shared2[sr][sc] = ? mat2[(s * BLOCK_SIZE + sr) * width + c] : 0.0f;
+    Ashared[row][col] = r < C.height and c < get_element(Asub, row, col);
+    Bshared[row][col] = get_element(Bsub, row, col);
 
-    // Wait for all other threads to finish filling to fully utilize shared memory
+    // Synchronize to make sure the sub-matrices are loaded before starting computation
     __syncthreads();
 
-    // Start sub matrix multiplication and accumulate
+    // Multiply Ashared and Bshared
+    for (int e = 0; e < BLOCK_SIZE; ++e)
+      Cvalue += Ashared[row][e] * Bshared[e][col];
 
-    for (int i = 0; i < BLOCK_SIZE; ++i)
-      acc += shared1[sr][i] * shared2[i][sc];
-
-    // Wait for all threads to finish computing before moving to the next tile
+    // Synchronize to make sure the preceding computation is done before loading
+    // two new sub-matrices of A and B in the next iteration
     __syncthreads();
   }
 
   if (r < height && c < width)
-    output[r * width + c] = acc;
+    set_element(Csub, row, col, Cvalue);
 }
 
 torch::Tensor matmul_shared(torch::Tensor mat1, torch::Tensor mat2)
